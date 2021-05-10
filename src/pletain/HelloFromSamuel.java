a2c2dbd370bb0a76ec972d01acaae4035222ec17
unknown <samuel.pletain@gmail.com>
2021-05-11 01:33:46 +0200

    Initial commit

---
+++ src/pletain/HelloFromSamuel.java
@@ -0,0 +1,9 @@
+package pletain;
+
+public class HelloFromSamuel {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, my name is Samuel.");
+    }
+}
