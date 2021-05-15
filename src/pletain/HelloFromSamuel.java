845087f62cc3c16ce3657792f3fd8062b2418426
Daniel Martinez <84058971+Danii89Martz@users.noreply.github.com>
2021-05-15 14:43:45 -0400

    daniel

--- src/pletain/HelloFromSamuel.java
+++ src/pletain/HelloFromSamuel.java
@@ -5,5 +5,6 @@ public class HelloFromSamuel {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Samuel.");
+        System.out.println("Hello Samuel, my name is Daniel.");
     }
 }
